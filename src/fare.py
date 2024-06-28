d266481ddc7dda06b2bba0df9d5f4a96777046ec
AndreaMoraledaBlancom <andreamoraleda@gmail.com>
2024-06-28 14:47:22 +0200

    fix the last change

--- src/fare.py
+++ src/fare.py
@@ -1,8 +1,6 @@
 # Creamos la clase Tarifa que recibira los precios 
 class Fare:
-    # Constructor que tiene los parámetros de las tarifas stop y movement
+    # Constructor que tiene los argumentos de las tarifas stop y movement
     def __init__(self, stop_fare, movement_fare):
         self.stop_fare = stop_fare
         self.movement_fare = movement_fare
-
-# OPCIONAL MÉTODO PARA MODIFICAR TARIFA
